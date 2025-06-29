576e7214d5a344ad6e597a833a2061dbc6052b66
Raul1105 <raul.souzal@hotmail.com>
2025-06-29 01:32:32 -0300

    Segundo exercicio

---
+++ PrimeiroProjeto/src/SegundoExercicio/java/Programa1.java
@@ -0,0 +1,11 @@
+package SegundoExercicio.java;
+
+public class Programa1 {
+    public static void main(String[] args) {
+        int valor = 10;
+        Integer valor2 = valor;
+
+        System.out.println("Valor de int: " + valor);
+        System.out.println("Valor de Integer: " + valor2);
+    }
+}
