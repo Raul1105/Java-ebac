9c8cc6b7553d12d6574ddbbdbfa5eaad34e48e98
Raul1105 <113746444+Raul1105@users.noreply.github.com>
2025-07-03 02:00:57 -0300

    Create Pessoas.java

---
+++ Exemplos/src/Pessoas.java
@@ -0,0 +1,30 @@
+public class Pessoas{
+    
+    private String nome;
+    private String sexo;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+    
+    public String getSexo(){
+        return sexo;
+    }
+    
+    public Pessoas(String nome, String sexo){
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+    
+    public String toString(){
+        return nome;
+    }
+}
