00fd69254f610a8af3306ff5e9d0fa6cf6d06601
Raul1105 <113746444+Raul1105@users.noreply.github.com>
2025-07-04 02:03:21 -0300

    Create QuartoExercicio.java

---
+++ PrimeiroProjeto/src/QuartoExercicio/QuartoExercicio.java
@@ -0,0 +1,12 @@
+public class QuartoExercicio {
+
+    public static void main(String[] args) {
+        Notas aluno = new Notas(2.65,8.98,5.21,8.98);
+        aluno.calcularMedia();
+
+        Notas aluno2 = new Notas(1.25,6.85,9.99,10.0);
+        aluno2.calcularMedia();
+
+
+    }
+}
