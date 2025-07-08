794ddeab1327a4ffddc935202377bcf3861d3f4b
Raul1105 <113746444+Raul1105@users.noreply.github.com>
2025-07-08 03:09:28 -0300

    Update NonoExercicio.java

--- PrimeiroProjeto/src/NonoExercicio/NonoExercicio.java
+++ PrimeiroProjeto/src/NonoExercicio/NonoExercicio.java
@@ -6,7 +6,7 @@ public class NonoExercicio{
 	}
 	
 	public static void exercicio9(){
-	    String[] nomes = {"Raul - masculino", "Larissa - feminino", "Lorena - feminino", "Velton - masculino"};
+	    String[] nomes = {"Raul - masculino", "Larissa - feminino", "Lorena - feminino", "Joao - masculino"};
 		List<Pessoas> pessoas = new ArrayList<>();
 		
 		for(String pessoa : nomes){
@@ -17,5 +17,34 @@ public class NonoExercicio{
 		for(Pessoas pessoa : pessoas){
 		    System.out.println("Nome:" + pessoa.getNome() + " sexo:" + pessoa.getSexo());
 		}
+		
+		List<Pessoas> homens = new ArrayList<>();
+    	List<Pessoas> mulheres = new ArrayList<>();
+    	
+    	for(Pessoas pessoa : pessoas){
+    	    if(pessoa.getSexo().equals("masculino")){
+    	        homens.add(pessoa);
+    	    }
+    	}
+    	
+    	for(Pessoas pessoa : pessoas){
+    	    if(pessoa.getSexo().equals("feminino")){
+    	        mulheres.add(pessoa);
+    	    }
+    	}
+    	
+    	System.out.println("***Lista de homens***");
+    	
+    	for(Pessoas homem : homens){
+    	    System.out.println(homem);
+    	}
+    	
+    	System.out.println("***Lista de mulheres");
+    	
+    	for(Pessoas mulher : mulheres){
+    	    System.out.println(mulher);
+    	}
 	}
+	
+	
 }
